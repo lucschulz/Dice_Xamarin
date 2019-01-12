589e6ad981c0f80d33b3d3a211865d2cde9800e9
Luc Schulz <luc@lucschulz.com>
2019-01-12 13:43:45 -0500

    LS

--- Dice/Die.cs
+++ Dice/Die.cs
@@ -12,24 +12,82 @@ using Android.Widget;
 
 namespace Dice
 {
+    public enum DieColor
+    {
+        White,
+        Transparent
+    }
+
     class Die
     {
+        private Random random;
         private int dieValue;
-        private DieColors dieColor;
+        private DieColor currentDieColor;
         private ImageView imageView;
 
-        public Die(ImageView imageView, DieColors color)
+        public Die(ImageView imageView, DieColor color)
         {
+            random = new Random();
+
             this.imageView = imageView;
-            this.dieColor = color;
-            rollDie();
+            this.currentDieColor = color;
+            RollDie();
         }
 
-
-
-        private void rollDie()
+        public void SetDieColor(DieColor dieColor)
         {
-            throw new NotImplementedException();
+            this.currentDieColor = dieColor;
+            SetDieValue(imageView, dieValue);
+        }
+
+        private void RollDie()
+        {            
+            int dieValue = random.Next(1, 7);
+        }
+
+        public bool IsVisible()
+        {
+            ViewStates visibility = imageView.Visibility;
+            if (visibility == ViewStates.Visible)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void SetVisible(bool visible)
+        {
+            if (visible)
+            {
+                imageView.Visibility = ViewStates.Visible;
+            }
+            else
+            {
+                imageView.Visibility = ViewStates.Invisible;
+            }
+
+        }
+
+        private void SetDieValue(ImageView dieImg, int value)
+        {
+            if (currentDieColor == DieColor.White)
+            {
+                if (value == 1)
+                    dieImg.SetImageResource(Resource.Drawable.die1_white);
+
+                else if (value == 2)
+                {
+                    dieImg.SetImageResource(Resource.Drawable.die2_white);
+                }
+            }
+
+            else if (currentDieColor == DieColor.Transparent)
+            {
+
+            }
         }
     }
 }
--- Dice/DieColors.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Dice
-{
-    internal class DieColors
-    {
-    }
-}
